016aa83e8dcde8b27d99c46620db16f44455cdfc
iosアカデミア <yutamac2512@icloud.com>
2025-03-14 19:53:29 +0900

    最初のコミット

---
+++ FirebasePracticeApp/FirebasePracticeApp/FirebasePracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeAppApp.swift
+//  FirebasePracticeApp
+//
+//  Created by 俺の MacBook Air on 2025/03/14.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
